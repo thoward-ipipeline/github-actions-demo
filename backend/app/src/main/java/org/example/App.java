d73c3ee39297ef839204d035e00cf92463e8e88d
Tyler Howard <tyler.howard@ipipeline.com>
2025-02-28 00:21:25 -0500

    fix java quote and make the code ugly

--- backend/app/src/main/java/org/example/App.java
+++ backend/app/src/main/java/org/example/App.java
@@ -4,11 +4,4 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
-}
+    public String getGreeting() { return "Hello World!"; } public static void main(String[] args) { System.out.println(new App().getGreeting()); } }
