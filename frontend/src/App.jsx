e635be70df4da35371931d2695c5c2bf3172fc74
Tyler Howard <tyler.howard@ipipeline.com>
2025-03-03 12:04:37 -0500

    break feature branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+function App() 
   const [count, setCount] = useState(0);
 
   console.log("hello world but enhanced.okay maybe it works this time");
